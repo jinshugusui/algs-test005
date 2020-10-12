d47f72811c6df28f5108ef5b7bbc4360078abebf
jinshugusui <291691357@qq.com>
2020-10-12 22:27:06 +0800

    Update SelectSore.java

--- src/com/company/sort/SelectSore.java
+++ src/com/company/sort/SelectSore.java
@@ -4,5 +4,6 @@ public class SelectSore {
     public static void main(String[] args) {
         System.out.println("hello haha ");
         //say big big world 
+        // welcome to china
     }
 }
