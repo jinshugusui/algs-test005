b7de75f76eee15db289641fec8fad11981dd73dc
chenshijie <291691357@qq.com>
2020-10-13 21:59:42 +0800

    use idea commit project to github001```

---
+++ src/com/company/sort/InsertSort.java
@@ -0,0 +1,23 @@
+package com.company.sort;
+
+import com.company.util.PrintArray;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] a = {0,1,9,2,8,3,7,4,6,5};
+        PrintArray.printArray(a);
+
+        insertSort(a);
+
+        PrintArray.printArray(a);
+
+    }
+
+    public static void insertSort(int[] a) {
+        int N = a.length;
+        for (int i = 0; i < N - 1; i++) {
+
+        }
+
+    }
+}
--- src/com/company/sort/SelectSort.java
+++ src/com/company/sort/SelectSort.java
@@ -1,23 +1,33 @@
 package com.company.sort;
 
+import com.company.util.Compare;
+import com.company.util.Exch;
 import com.company.util.PrintArray;
 
 public class SelectSort {
 
 
     public static void main(String[] args) {
-        int[] a = {0,1,9,2,8,3,7,4,6,5};
+        int[] a = {9,8,7,6,5,4,3,2,1,0};
         PrintArray.printArray(a);
 
         selectSort(a);
 
-        //say hello
-        //say hi hello world
         PrintArray.printArray(a);
     }
 
 
     public static void selectSort(int[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min  = i;
+            for (int j = i; j < N; j++) {
+                if (a[min] > a[j]) {
+                    min = j;
+                }
+            }
+            Exch.exch(a, i, min);
+        }
 
     }
 
