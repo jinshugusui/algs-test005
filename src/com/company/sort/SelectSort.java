82ddd45ac305b0461079990fa936804e49c89618
chenshijie <291691357@qq.com>
2020-10-13 21:21:16 +0800

    use idea commit project to github001```

--- src/com/company/sort/SelectSort.java
+++ src/com/company/sort/SelectSort.java
@@ -1,13 +1,26 @@
 package com.company.sort;
 
+import com.company.util.PrintArray;
+
 public class SelectSort {
+
+
     public static void main(String[] args) {
-        System.out.println("hello haha ");
-        //say big big world 
-        // welcome to china
-        //hello
-        //eclipse
-        //hi boy welcome to beijing
-        //good good
+        int[] a = {0,1,9,2,8,3,7,4,6,5};
+        PrintArray.printArray(a);
+
+        selectSort(a);
+
+        //say hello
+        //say hi hello world
+        PrintArray.printArray(a);
     }
+
+
+    public static void selectSort(int[] a) {
+
+    }
+
+
+
 }
