619db7fe2f9127d0fe842f7003a1b039b00c1a8b
chenshijie <291691357@qq.com>
2020-10-14 20:30:51 +0800

    use idea commit project to github001```

---
+++ src/com/company/sort/ShellSort.java
@@ -0,0 +1,21 @@
+package com.company.sort;
+
+import com.company.util.PrintArray;
+
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] a = {0,1,9,2,8,3,7,4,6,5};
+        PrintArray.printArray(a);
+
+        shellSort(a);
+
+        PrintArray.printArray(a);
+
+
+    }
+
+
+    public static void shellSort(int[] a) {
+
+    }
+}
