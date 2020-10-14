508e50be30aff1412d9d93f113ee118454dd4e27
jinshugusui <291691357@qq.com>
2020-10-14 17:19:53 +0800

    1111

--- src/com/company/sort/InsertSort.java
+++ src/com/company/sort/InsertSort.java
@@ -1,5 +1,6 @@
 package com.company.sort;
 
+import com.company.util.Compare;
 import com.company.util.PrintArray;
 
 public class InsertSort {
@@ -15,9 +16,28 @@ public class InsertSort {
 
     public static void insertSort(int[] a) {
         int N = a.length;
-        for (int i = 0; i < N - 1; i++) {
+        for (int i = 1; i < N; i++) {
+        	int index = i;
+        	int temp = a[index];
 
+        	for (int j = i; j-1 >= 0; j--) {
+        		if (temp - a[j-1] < 0) {
+        			index = j-1;
+        			a[j] = a[j-1];
+        		}
+			}
+        	a[index] = temp;
         }
-
     }
 }
+
+
+
+
+
+
+
+
+
+
+
